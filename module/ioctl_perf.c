ab030249faebdd42ef1dc8853b75eed281b04fc0
Damien Carver <carverdamien@gmail.com>
2019-01-09 10:34:12 +0100

    include linux/sched/clock.h for local_clock() definition

--- module/ioctl_perf.c
+++ module/ioctl_perf.c
@@ -14,6 +14,7 @@
 #include <linux/list.h>
 #include <linux/mm.h>
 #include <linux/sched.h>
+#include <linux/sched/clock.h>
 #include <linux/fs.h> // required for various structures related to files liked fops. 
 #include <linux/semaphore.h>
 #include <linux/cdev.h> 
